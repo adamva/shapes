12a141c70c6ff04f0fd69aa4115d066a12379bd3
Adam A <adamva@outlook.com>
2020-02-13 11:18:31 -0700

    Add navbar

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,6 @@
-alert('hi')
+//create square, circle, star, and rectangle shape
+//create shape when user clicks
+//associate shape with keyboard
+//change shape when user presses key
+//associate color with shapes
+//change color when user presses key
