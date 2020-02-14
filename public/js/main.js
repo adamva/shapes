258a761e898cfb562683a10683d5a91e13680fb6
Adam A <adamva@outlook.com>
2020-02-14 14:10:01 -0700

    add color picker

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,3 @@
-//create square, circle, star, and rectangle shape
-//create shape when user clicks
-//associate shape with keyboard
-//change shape when user presses key
-//associate color with shapes
-//change color when user presses key
 
 window.globals = { 
     shape: 'square', 
